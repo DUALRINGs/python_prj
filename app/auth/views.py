25e0669d5b05400f171484e76d6ca5322e8aaab5
root <root@IVANPK.tufar.gov.spb.ru>
2025-01-22 13:51:12 +0300

    fix

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,22 +6,20 @@ from users.schemas import User, UserResponse, UserUpdatePartial, User
 from . import utils
 from fastapi.responses import RedirectResponse
 from .dependencies import *
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import OAuth2PasswordBearer
 from authlib.jose import JsonWebToken, errors
 
 router = APIRouter(prefix="/login")
 
 
 # Схема OAuth2 для извлечения токена из заголовка Authorization
-http_bearer = HTTPBearer()
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/login")
 
 async def get_current_token_payload(
-    credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+    token: str = Depends(oauth2_schema),
 ) -> dict:
     token = credentials.credentials 
-    """
-    Извлекает и декодирует JWT-токен, возвращая его payload.
-    """
+
     try:
         # Декодируем токен (если decode_jwt асинхронная, добавьте await)
         payload = await utils.decode_jwt(token=token)
@@ -61,9 +59,7 @@ async def get_current_auth_user(
     session: AsyncSession = Depends(helper.session_dependency),
     payload: dict = Depends(get_current_token_payload),
 ) -> User:
-    """
-    Извлекает пользователя из базы данных на основе данных из JWT-токена.
-    """
+
     email: str | None = payload.get("email")
     if user := await user_by_email(session=session, email=email):
         return user
@@ -76,11 +72,9 @@ async def auth_user_issue_jwt(
     user: User = Depends(validate_auth_user),
 ):
     jwt_payload = {
-        # subject
         "sub": user.name,
         "username": user.name,
         "email": user.email,
-        # "logged_in_at"
     }
     token = await utils.encode_jwt(jwt_payload)
    #return RedirectResponse(url="/users", status_code=status.HTTP_303_SEE_OTHER)
