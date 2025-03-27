84c7629fb0b54d9d641c66a9b85f821c1fbf5003
root <root@IVANPK.tufar.gov.spb.ru>
2025-03-27 17:37:14 +0300

    add confest.py + test_tasks_crud.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,56 +1,5 @@
-import asyncio
-
 import pytest
-import pytest_asyncio
-from httpx import AsyncClient, ASGITransport
-from app.main import app
-
-
-@pytest_asyncio.fixture(scope="session")
-def event_loop():
-    """Использование одного event loop для всех тестов."""
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest_asyncio.fixture
-async def client() -> AsyncClient:
-    """
-    Фикстура для создания асинхронного HTTP-клиента.
-    """
-    async with AsyncClient(
-            transport=ASGITransport(app=app),
-            base_url="http://test",
-    ) as client:
-        yield client
-
-
-@pytest_asyncio.fixture
-async def user_data() -> dict:
-    """
-    Фикстура с данными для создания пользователя.
-    """
-    return {
-        "name": "testuser",
-        "email": "testtest@example.com",
-        "password": "test_password",
-    }
-
-
-@pytest_asyncio.fixture
-async def auth(client: AsyncClient, user_data: dict) -> dict:
-    """
-    Фикстура для аутентификации пользователя и получения токена.
-    """
-    # Создание пользователя
-    await client.post("/auth/register", json=user_data)
-
-    # Аутентификация и получение токена
-    response = await client.post("/auth/login", data={"username": user_data["email"], "password": user_data["password"]})
-
-    assert response.status_code == 200, f"Authentication failed: {response.text}"
-    return response.json()  # Токен будет в ответе, например, response.json()["access_token"]
+from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
@@ -68,7 +17,6 @@ async def test_register_user(client: AsyncClient, user_data: dict):
     assert "email" in response_data
     assert response_data["email"] == user_data["email"]
 
-
 @pytest.mark.asyncio
 async def test_login_user(client: AsyncClient, user_data: dict):
     """
@@ -78,8 +26,7 @@ async def test_login_user(client: AsyncClient, user_data: dict):
     await client.post("/auth/register", json=user_data)
 
     # Пытаемся аутентифицироваться
-    response = await client.post("/auth/login",
-                                 data={"username": user_data["email"], "password": user_data["password"]})
+    response = await client.post("/auth/login", data={"username": user_data["email"], "password": user_data["password"]})
 
     # Проверяем, что статус код 200
     assert response.status_code == 200, f"Login failed: {response.text}"
@@ -89,14 +36,12 @@ async def test_login_user(client: AsyncClient, user_data: dict):
     assert "access_token" in response_data
     assert response_data["token_type"] == "bearer"
 
-
 @pytest.mark.asyncio
-async def test_protected_endpoint(client: AsyncClient, auth: dict):
+async def test_protected_endpoint(client: AsyncClient, token: str):
     """
     Тест доступа к защищенному эндпоинту.
     """
     # Получаем токен из фикстуры auth
-    token = auth["access_token"]
 
     # Делаем запрос к защищенному эндпоинту с токеном
     response = await client.get("/users/me", headers={"Authorization": f"Bearer {token}"})
---
+++ tests/test_tasks_crud.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_create_task(client: AsyncClient, task_data: dict, token: str):
+    response = await client.post(
+    "/tasks",
+    headers={"Authorization": f"Bearer {token}"},
+    json=task_data,
+    )
+    assert response.status_code == 200
